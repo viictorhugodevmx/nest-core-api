f93b8d2772749fc4808abd6f8b4c803083d2a22b
Victor Hugo Segundo Aguilar <victorhsegundoa@gmail.com>
2026-07-13 21:13:56 -0600

    [UPDATE] - Adding connection mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,35 @@
 import { Module } from '@nestjs/common';
+import {
+  ConfigModule,
+  ConfigService
+} from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
-import { OpportunitiesModule } from './modules/opportunities/opportunities.module';
-import { StatusModule } from './modules/status/status.module';
 import { CompaniesModule } from './modules/companies/companies.module';
-import { RecruitersModule } from './modules/recruiters/recruiters.module';
 import { InterviewNotesModule } from './modules/interview-notes/interview-notes.module';
+import { OpportunitiesModule } from './modules/opportunities/opportunities.module';
+import { RecruitersModule } from './modules/recruiters/recruiters.module';
+import { StatusModule } from './modules/status/status.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+
+    MongooseModule.forRootAsync({
+      inject: [
+        ConfigService
+      ],
+      useFactory: (
+        configService: ConfigService
+      ) => ({
+        uri: configService.getOrThrow<string>(
+          'MONGODB_URI'
+        )
+      })
+    }),
+
     StatusModule,
     OpportunitiesModule,
     CompaniesModule,
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+
+  const port = configService.get<number>(
+    'PORT',
+    3004
+  );
 
   app.setGlobalPrefix('api');
 
@@ -16,7 +24,7 @@ async function bootstrap(): Promise<void> {
     })
   );
 
-  await app.listen(3004);
+  await app.listen(port);
 }
 
 void bootstrap();
